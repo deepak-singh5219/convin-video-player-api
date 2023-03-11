ed7a799a29c5c9d91d2749d86fa443bfa37b477a
Deepak Singh <singhdeepak5219@gmail.com>
2023-03-11 17:55:25 +0530

    initialiesed app

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+require('./config/database').connect();
+const express = require('express');
+const app = express();
+
+const router = require('./routes/router');
+app.use(express.json()); // to use json inside the express application.
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use('/',router); // used for routing
+module.exports = app;
